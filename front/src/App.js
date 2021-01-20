db5c0390c73540a2e1e6a22839d7963c94f5d41c
Alexis Heroin <alexis.heroin@epsi.fr>
2021-01-20 19:24:13 +0100

    test modification docker-compose pour bdd

--- front/src/App.js
+++ front/src/App.js
@@ -15,7 +15,7 @@ function App() {
     const [selectedType, setSelectedType] = useState("all");
 
     useEffect(() => {
-        fetch("/api/question")
+        fetch("http://localhost:5000/api/question")
             .then(res => res.json())
             .then(
                 (result) => {
