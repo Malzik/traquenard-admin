1757a4658edf8481ece0de09fa79b780d7b473e6
Alexis Heroin <alexis.heroin@epsi.fr>
2021-01-20 13:10:35 +0100

    update db creation

--- database/db.sql
+++ database/db.sql
@@ -5,6 +5,8 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+CREATE DATABASE IF NOT EXISTS `traquenard`;
+
 USE `traquenard`;
 
 SET NAMES utf8mb4;
